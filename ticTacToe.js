ddaceeedb96989484aa8fc086e1ea28cea96727c
Ruperto Solis <ruperto@Rupertos-MacBook-Pro.local>
2019-03-05 21:00:46 -0800

    Second commit

--- ticTacToe.js
+++ ticTacToe.js
@@ -12,6 +12,10 @@ function mark(cubeNumber, cubesArray){
       //Nothing happens
     }
   }
+  var winner = checkWinner(cubesArray, cubeNumber);
+  if (winner){
+    alert("WINNER");
+  }
 }
 
 function startGame(cubesArray){
@@ -21,9 +25,39 @@ function startGame(cubesArray){
   }
 }
 
-function checkWinner(){
+function checkWinner(cubesArray, boxNumber){
+  var winnerPaths=[];
+  for (var i=0; i<9; i++){
+    winnerPaths[i]=[];
+  }
+  winnerPaths[0][0]="123";winnerPaths[0][1]="159";winnerPaths[0][2]="147";
+  winnerPaths[2][0]="123";winnerPaths[2][1]="357";winnerPaths[2][2]="369";
+  winnerPaths[6][0]="147";winnerPaths[6][1]="357";winnerPaths[6][2]="789";
+  winnerPaths[8][0]="369";winnerPaths[8][1]="159";winnerPaths[8][2]="789";
+  winnerPaths[4][0]="159";winnerPaths[4][1]="357";
+  winnerPaths[4][2]="258";winnerPaths[4][3]="456";
+  winnerPaths[1][0]="123";winnerPaths[1][1]="258";
+  winnerPaths[3][0]="147";winnerPaths[3][1]="456";
+  winnerPaths[5][0]="369";winnerPaths[5][1]="456";
+  winnerPaths[7][0]="789";winnerPaths[7][1]="258";
   //checks the winning combinations of the las clicked cell
   //and verifies if 3 cells of the same type are the same
+  var winner=false;
+  for (var i=0; i<winnerPaths[boxNumber-1].length; i++){
+    var numbers=[];
+    numbers[0]=winnerPaths[boxNumber-1][i][0];
+    numbers[1]=winnerPaths[boxNumber-1][i][1];
+    numbers[2]=winnerPaths[boxNumber-1][i][2];
+    // console.log(numbers[0]);
+    // console.log(numbers[1]);
+    // console.log(numbers[2]);
+    if (cubesArray[numbers[0]-1].innerText == cubesArray[numbers[1]-1].innerText &&
+        cubesArray[numbers[1]-1].innerText == cubesArray[numbers[2]-1].innerText &&
+        cubesArray[numbers[0]-1].innerText != ""){
+          winner=true;
+        }
+  }
+  return winner;
 }
 var oneColor=false;
 var twoColor=false;
